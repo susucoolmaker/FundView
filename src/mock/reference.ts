d831b19bbb84f8bc7ee79ba950adad7c281070d2
susucoolmaker <susualine527@gmail.com>
2026-07-24 15:50:29 +0800

    Delete src directory

--- src/mock/reference.ts
+++
@@ -1,1 +0,0 @@
-export const mockReferenceDate = new Date('2026-07-17T12:00:00+08:00')
--- src/tests/setup.ts
+++
@@ -1,6 +0,0 @@
-import { vi } from 'vitest'
-
-Object.defineProperty(window, 'scrollTo', {
-  value: vi.fn(),
-  writable: true,
-})
